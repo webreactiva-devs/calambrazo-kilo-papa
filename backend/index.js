21b643d12eca542c3e8e644f0a463f9e9635f00e
martachips <martachips@gmail.com>
2025-02-17 13:23:29 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,10 @@ connectDB();
 
 app.use(express.json());
 app.use('/api', questionsRoutes);
-app.use(express.static(path.join(__dirname, 'frontend')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'frontend')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
