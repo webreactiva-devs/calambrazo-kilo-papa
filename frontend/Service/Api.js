eaa60eea73b74095bb92ca3bab8a10255084e77c
martachips <martachips@gmail.com>
2025-02-17 13:58:36 +0100

    enlace a back

--- frontend/Service/Api.js
+++ frontend/Service/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://calambrazo-kilo-papa.onrender.com';
 
 export const fetchQuestions = async () => {
   const response = await fetch(`${BASE_URL}/questions`);
