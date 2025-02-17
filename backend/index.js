bd8e8e08fbec4ab6979ee50017bd9c6f8a6471f1
martachips <martachips@gmail.com>
2025-02-17 10:35:45 +0100

    carpeta public

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ connectDB();
 
 app.use(express.json());
 app.use('/api', questionsRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
--- backend/public/main.js
+++ backend/public/main.js
@@ -1,3 +1,3 @@
-import { initApp } from './App.js';
+import { initApp } from '../../frontend/App.js';
 
 document.addEventListener('DOMContentLoaded', initApp);
